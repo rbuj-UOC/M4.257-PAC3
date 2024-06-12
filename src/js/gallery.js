838e80d63f78e1718f8b0f471f8e3bce924fcea9
rbuj <robert.buj@gmail.com>
2024-06-12 11:05:04 +0200

    initial commit

---
+++ src/js/gallery.js
@@ -0,0 +1,10 @@
+import PhotoSwipeLightbox from 'photoswipe/dist/photoswipe-lightbox.esm.js';
+import 'photoswipe/dist/photoswipe.css';
+
+const lightbox = new PhotoSwipeLightbox({
+    gallery: '#gallery--zoom-transition',
+    children: 'a',
+    showHideAnimationType: 'zoom',
+    pswpModule: () => import('photoswipe/dist/photoswipe.esm.js')
+});
+lightbox.init();
---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// index.js
+import wait from "waait";
+
+async function go() {
+  console.log("Going!");
+  await wait(200);
+  console.log("Ending!");
+}
+
+go();
